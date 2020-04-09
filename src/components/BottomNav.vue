cb5dc0a9cf09035cdcd1343c0babe508988d93f8
Ollebacx <dav.ollebac@gmail.com>
2020-04-09 05:20:48 +0100

    WIP Workouts view

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-bottom-navigation :value="activeBtn" grow color="white" dark fixed bottom>
+    <v-btn>
+      <span>Inicio</span>
+      <v-icon>mdi-home-variant</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Workouts</span>
+      <v-icon>mdi-dumbbell</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Actividad</span>
+      <v-icon>mdi-poll</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: "BottomNav",
+  data() {
+    return {
+      activeBtn: 1
+    };
+  }
+};
+</script>
---
+++ src/views/Workouts.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-container class="structure">
+      <v-row>
+        <v-col>
+          <div class="myWorkouts"></div>
+          <v-img
+            src="https://images.unsplash.com/photo-1585342565162-3704ff9b221d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+            aspect-ratio="2"
+          >
+            <p>Crear entrenamiento personalizado</p>
+          </v-img>
+          <v-img
+            src="https://images.unsplash.com/photo-1434682881908-b43d0467b798?ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80"
+            aspect-ratio="2"
+          >
+            <p>Ejercicios individuales</p>
+          </v-img>
+        </v-col>
+      </v-row>
+      <Bottom-Nav />
+    </v-container>
+  </div>
+</template>
+
+<script>
+import BottomNav from "../components/BottomNav";
+export default {
+  name: "Workouts",
+  data() {
+    return {};
+  },
+  components: {
+    BottomNav
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0px !important;
+  padding: 0px !important;
+}
+.structure {
+  background-color: #262626;
+  max-height: 100vh;
+  min-height: 100vh;
+  text-align: center;
+  * {
+    color: white;
+    text-decoration-line: none;
+    font-family: "Montserrat", sans-serif;
+  }
+  p {
+    text-align: left;
+    width: 85%;
+    margin: 20px auto !important;
+    font-family: "Russo One", sans-serif;
+    font-size: 24px;
+  }
+}
+.myWorkouts {
+  height: 220px;
+  background-color: #ffcb15;
+}
+</style>
