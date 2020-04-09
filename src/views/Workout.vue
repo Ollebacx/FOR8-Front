364bac8b07850c363ba7149575df50eeb017e274
Ollebacx <dav.ollebac@gmail.com>
2020-04-10 00:01:04 +0100

    Workout > Exercices list

--- src/views/Workout.vue
+++ src/views/Workout.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-app-bar fixed color="#1A1A1A" dense dark>
+    <v-app-bar color="#1A1A1A" dense dark>
       <v-btn icon>
         <router-link to="/workouts/mis-workouts">
           <v-icon>mdi-arrow-left</v-icon>
@@ -13,16 +13,29 @@
         <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-expansion-panels>
-      <v-expansion-panel v-for="exercice in workout.exercises" :key="exercice">
-        <v-expansion-panel-header>{{ exercise }}</v-expansion-panel-header>
-        <v-expansion-panel-content>
-          {{ workout }}
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-    <!-- {{ workout.exercises}} -->
-    <BottomNav />
+    <!-- {{ workout.exercises }} -->
+    <v-content>
+      <v-expansion-panels accordion tile dark>
+        <v-expansion-panel
+          v-for="exercise in workout.exercises"
+          :key="exercise._id"
+        >
+          <v-expansion-panel-header>{{
+            exercise.name
+          }}</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list-item-avatar tile width="100%" height="150px"
+              ><v-img :src="exercise.photo_url" alt="" id="img" height="auto"
+            /></v-list-item-avatar>
+            <p>{{ exercise.description }}</p>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-content>
+    <v-footer app class="pa-0">
+      <v-btn x-large block color="#FFCB15">EMPEZAR</v-btn>
+      <BottomNav />
+    </v-footer>
   </div>
 </template>
 
@@ -57,4 +70,16 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+* {
+  color: white !important;
+  text-decoration-line: none;
+  font-family: "Montserrat", sans-serif;
+  margin: 0px !important;
+}
+p {
+  font-size: 14px;
+  margin-top: 10px !important;
+  color: rgb(245, 245, 245) !important;
+}
+</style>
